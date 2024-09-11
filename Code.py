cb54a9473883216abad4caa822e1be0408a53222
Nikunj Dhiman <147308602+Nikunj1724@users.noreply.github.com>
2024-09-11 15:09:11 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -4,37 +4,41 @@ import Library
 import pandas as pd
 import numpy as np
 import dataset
-df = pd.read_csv(r"https://raw.githubusercontent.com/YBI-Foundation/Dataset/Main/Financial%20Market%20News.csv", encoding = 'ISO-8859_1')
+# Use the raw URL to access the raw CSV data
+df = pd.read_csv("https://raw.githubusercontent.com/YBI-Foundation/Dataset/main/Financial%20Market%20News.csv", encoding = 'ISO-8859_1') 
+
 df.head()
 df.info()
 df.shape
 df.columns
-                                                                                                  
-Get Feature Selection
-' '. join(str(x) for x in df.iloc[1,2:27])
+
+#Get Feature Selection
+' '.join(str(x) for x in df.iloc[1,2:27]) 
 df.index
 len(df.index)
-news = []
+news = [] 
+for row in range(0,len(df.index)):
+  news.append(' '.join(str(x) for x in df.iloc[row,2:27])) # Join the text data from columns 2 to 26 into a single string for each row
 type(news)
-news[0]
+
 x = news
 type(x)
-     
-Get Feature Text Conversion to Bag of Words
+
+#Get Feature Text Conversion to Bag of Words
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(lowercase = True, ngram_range=(1,1))
 x = cv.fit_transform(x)
 x.shape
-y = df['label']
+y = df['Label'] # Use the correct column name "Label"
 y.shape
-     
-Get Train Test Split
+
+#Get Train Test Split
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train _test_split(x,y,test_size = 0.3, stratify = y, random_state = 2529)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.3, stratify = y, random_state = 2529)
 from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier(n_estimators=200)
 rf.fit(x_train, y_train)
 y_pred = rf.predict(x_test)
-from sklearn.metrics import Classification report , confusion matrix, accuracy score
-confusion_matric(y_test, y_pred)
-print(Clasification_report(y_test, y_pred)
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+confusion_matrix(y_test, y_pred)
+print(classification_report(y_test, y_pred))
